ed7420871fe8cae830864832e420c7d0d555326b
unknown <marcinxmurach995@gmail.com>
2019-04-02 05:05:59 +0200

    google-auth

--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
 <?php
-	if(!isset($_SESSION)) { session_start(); } 
-	echo "Witam!";
+	require_once __DIR__ . '../../vendor/autoload.php';
+
+	use Config\GoogleClient;
+
+	$googleClient = new GoogleClient();
+	$client = $googleClient->getClient();
+	$loginURL = $client->createAuthurl();
 ?>
+
+<html>
+	<head>
+		<title>Google Auth Test</title>
+		<link rel="icon" type="image/png" href=""/>
+	</head>
+	<body>
+		<form>
+			<input type="button"> onclick="window.location = '<?php echo $loginURL; ?>'" Google+ </input>
+		</form>
+		<img src="/grafika/wyk6_1a.png">
+	</body>
+</html>
