aa6612222a8d8df4c976bafe9a3449763a5f6b38
unknown <marcinxmurach995@gmail.com>
2019-04-02 19:11:52 +0200

    google-auth

--- public/index.php
+++ public/index.php
@@ -212,7 +212,7 @@
   		}
 		if(isset($_SESSION['token'])) echo '
 		<section class="userData" id="userData">
-			<div>
+			<div class="main">
 				<h2>Dane użytkownika:</h2>
 				<table>
   					<tr>
@@ -229,5 +229,21 @@
   			</div>
   		</section>';
   		?>
+  	<script>
+		var dropdown = document.getElementsByClassName("dropdown-btn");
+		var i;
+
+		for (i = 0; i < dropdown.length; i++) {
+ 			dropdown[i].addEventListener("click", function() {
+  				this.classList.toggle("active");
+  				var dropdownContent = this.nextElementSibling;
+  				if (dropdownContent.style.display === "block") {
+  					dropdownContent.style.display = "none";
+  				} else {
+  					dropdownContent.style.display = "block";
+  				}
+  			});
+		}
+	</script>
 	</body>
 </html>
