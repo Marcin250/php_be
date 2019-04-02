bc57d24146e8f1a37ac36b0f8684f2c922bc4141
unknown <marcinxmurach995@gmail.com>
2019-04-02 05:21:20 +0200

    google-auth

--- config/google.php
+++ config/google.php
@@ -13,12 +13,19 @@
 		
 		public function getClient()
 		{
-			$this->$client = new Google_Client();
-			$this->$client->setApplicationName("Authorization_API");
-			$this->$client->setClientId(getenv('GOOGLE_ID'));
-			$this->$client->setClientSecret(getenv('GOOGLE_SECRET'));
-			$this->$client->setRedirectUri(getenv('APP_URL') . '/user/google-callback');
-			$this->$client->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
+			try
+			{
+				$this->$client = new Google_Client();
+				$this->$client->setApplicationName("Authorization_API");
+				$this->$client->setClientId(getenv('GOOGLE_ID'));
+				$this->$client->setClientSecret(getenv('GOOGLE_SECRET'));
+				$this->$client->setRedirectUri(getenv('APP_URL') . '/user/google-callback');
+				$this->$client->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
+			}
+			catch(Google_Service_Exception $exception)
+			{
+				echo "Connection error: " . $exception->getErrors();
+			}
 			return $this->client;
 		}
 	}
