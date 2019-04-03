0e768ba2710d8a47d3c6d8ca4dd3152e904cdc3a
unknown <marcinxmurach995@gmail.com>
2019-04-03 19:17:58 +0200

    back

---
+++ app/objects/Cache.php
@@ -0,0 +1,54 @@
+<?php
+	namespace App\Objects;
+
+	require_once __DIR__ . '../../../vendor/autoload.php';
+	use PDO;
+
+	class Cache
+	{
+		private $expirationTime;
+		private $cacheDirectory;		
+		
+		public function __construct()
+		{
+			$this->cacheDirectory = '../cache/';
+		}
+		
+		public function remember($dataIndex, $expirationTime)
+		{
+			$data = json_encode(array("message" => "Błąd."));
+			$this->expirationTime = $expirationTime;
+			$cacheFile = $this->cacheDirectory . $dataIndex;
+			$fileTime = (file_exists($cacheFile)) ? @filemtime($cacheFile) : 0;
+			if($fileTime == 0)
+				return false;
+			else
+			{
+				if((time() - $this->expirationTime) < $fileTime)
+					$data = $this->cacheRead($cacheFile);
+				else
+					return false;
+			}
+			return $data;
+		}
+
+		public function cacheRead($cacheFile)
+		{
+			if(file_exists($cacheFile))
+				return file_get_contents($cacheFile);
+			else
+				return json_encode(array("message" => "Błąd."));
+		}
+
+		public function cacheWrite($dataIndex, $data)
+		{
+			$cacheFile = $this->cacheDirectory . $dataIndex;
+			if(file_exists($cacheFile))
+				unlink($cacheFile);
+			$openFile = fopen($cacheFile, 'w');
+			fwrite($openFile, $data);
+			fclose($openFile);
+			return $this->cacheRead($cacheFile);
+		}
+	}
+?>
